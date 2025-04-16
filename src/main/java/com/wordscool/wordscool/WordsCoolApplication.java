f065c9c5aecdec5ad479f762523d6beee57624d3
yuanchongyang <yuan.chongyang@foxmail.com>
2025-04-16 18:31:54 +0800

    Initial commit

---
+++ src/main/java/com/wordscool/wordscool/WordsCoolApplication.java
@@ -0,0 +1,13 @@
+package com.wordscool.wordscool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordsCoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordsCoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wordscool/wordscool/WordsCoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wordscool.wordscool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordsCoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
